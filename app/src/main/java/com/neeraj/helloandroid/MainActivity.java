7f55ca6eb2f0cbd55eaf1b36572bfeb2abbec0c6
Neeraj Kumar <neeraj.csn@gmail.com>
2026-08-26 14:48:40 +0530

    Android App- basic text view app- first commit- master

---
+++ app/src/main/java/com/neeraj/helloandroid/MainActivity.java
@@ -0,0 +1,34 @@
+package com.neeraj.helloandroid;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        // Adding but functionality
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            TextView textView = findViewById(R.id.textView);
+            String text = String.valueOf(textView.getText()).replace("!", "");
+            textView.setText(String.format("%s again!", text));
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
